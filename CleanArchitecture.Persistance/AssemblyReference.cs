52e42a686e406518f0c40e130145bdf2876d63e8
haruntanerkirac <haruntanerkirac@gmail.com>
2025-08-23 15:21:18 +0300

    Add project files.

---
+++ CleanArchitecture.Persistance/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Persistance;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly assembly = typeof(Assembly).Assembly;
+}
