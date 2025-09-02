f7c6dafd1adb6e540bce85801b28b0391e9799da
haruntanerkirac <haruntanerkirac@gmail.com>
2025-09-02 23:28:30 +0300

    updates

--- CleanArchitecture.Application/AssemblyReference.cs
+++ CleanArchitecture.Application/AssemblyReference.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-namespace CleanArchitecture.Presentation
+namespace CleanArchitecture.Application
 {
     public static class AssemblyReference
     {
---
+++ CleanArchitecture.Application/Features/CarFeatures/Command/CreateCar/CreateCarCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Dtos;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Command.CreateCar
+{
+    public sealed class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, MessageResponse>
+    {
+        public async Task<MessageResponse> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            //
+            return new("Araç başarıyla üretildi");
+        }
+    }
+}
