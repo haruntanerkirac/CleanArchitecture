4a2c0066a90aa801a7284f90a0b5d4ae7cd4b457
haruntanerkirac <haruntanerkirac@gmail.com>
2025-09-02 23:41:37 +0300

    updates

---
+++ CleanArchitecture.Application/Services/ICarService.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Application.Features.CarFeatures.Command.CreateCar;
+
+namespace CleanArchitecture.Application.Services
+{
+    public interface ICarService
+    {
+        Task CreateAsync(CreateCarCommand request, CancellationToken cancellationToken);
+    }
+}
