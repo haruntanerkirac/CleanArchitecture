3610bf8516bd4a128d42e3f7ed12071a2f554b35
haruntanerkirac <haruntanerkirac@gmail.com>
2025-09-03 12:28:33 +0300

    updates

---
+++ CleanArchitecture.Persistance/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Features.CarFeatures.Command.CreateCar;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Persistence.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateCarCommand, Car>().ReverseMap();
+        }
+    }
+}
