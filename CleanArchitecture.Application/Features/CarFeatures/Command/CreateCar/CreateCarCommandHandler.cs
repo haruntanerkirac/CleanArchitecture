a4cf70bbbcd4bebdd8915a0577f3c38740927bf5
haruntanerkirac <haruntanerkirac@gmail.com>
2025-09-05 20:49:26 +0300

    updates

--- CleanArchitecture.Application/Features/CarFeatures/Command/CreateCar/CreateCarCommandHandler.cs
+++ CleanArchitecture.Application/Features/CarFeatures/Command/CreateCar/CreateCarCommandHandler.cs
@@ -15,7 +15,7 @@ namespace CleanArchitecture.Application.Features.CarFeatures.Command.CreateCar
         public async Task<MessageResponse> Handle(CreateCarCommand request, CancellationToken cancellationToken)
         {
             await _carService.CreateAsync(request,cancellationToken);
-            return new("Araç başarıyla üretildi");
+            return new("Araç başarıyla kaydedildi");
         }
     }
 }
